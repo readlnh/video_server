99a4d9ab90df4319563724960eea530ec8041d77
readlnh <readlnh@163.com>
2020-04-07 01:26:31 +0800

    add Video Test

--- api/dbops/api_test.go
+++ api/dbops/api_test.go
@@ -1,32 +1,38 @@
 package dbops
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func clearTables()  {
+var tempvid string
+
+func clearTables() {
 	dbConn.Exec("truncate users")
 }
 
-func TestMain(m *testing.M)  {
+func TestMain(m *testing.M) {
 	clearTables()
+	fmt.Printf("Hello Test\n")
 	m.Run()
 	clearTables()
 }
 
-func TestUserWorkFlow(t *testing.T)  {
+func TestUserWorkFlow(t *testing.T) {
 	t.Run("Add", testAddUser)
 	t.Run("Get", testGetUser)
 	t.Run("Del", testDeleteUser)
 	t.Run("Reget", testRegetUser)
 }
 
-func testAddUser(t *testing.T)  {
+func testAddUser(t *testing.T) {
 	err := AddUserCredential("test", "123")
 	if err != nil {
 		t.Errorf("Error of AddUser: %v", err)
 	}
 }
 
-func testGetUser(t *testing.T)  {
+func testGetUser(t *testing.T) {
 	pwd, err := GetUserCredential("test")
 	if pwd != "123" || err != nil {
 		t.Errorf("Error of GetUser: %v", err)
@@ -40,7 +46,7 @@ func testDeleteUser(t *testing.T) {
 	}
 }
 
-func testRegetUser(t *testing.T)  {
+func testRegetUser(t *testing.T) {
 	pwd, err := GetUserCredential("test")
 	if err != nil {
 		t.Errorf("Error of RegetUser: %v", err)
@@ -48,5 +54,43 @@ func testRegetUser(t *testing.T)  {
 
 	if pwd != "" {
 		t.Errorf("Deleting user test failed")
+	}
+}
+
+func TestVideoWorkFlow(t *testing.T) {
+	clearTables()
+	t.Run("AddUser", testAddUser)
+	t.Run("AddVideoInfo", testAddVideo)
+	t.Run("GetVideoInfo", testGetVideoInfo)
+	t.Run("DelVideoInfo", testDeleteVideoInfo)
+	t.Run("RegetVideoInfo", testRegetVideoInfo)
+}
+
+func testAddVideo(t *testing.T) {
+	videoInfo, err := AddNewVideo(1, "test_video")
+	if err != nil {
+		t.Errorf("Error of AddVideo: %v", err)
+	}
+	tempvid = videoInfo.Id
+}
+
+func testGetVideoInfo(t *testing.T) {
+	_, err := GetVideoInfo(tempvid)
+	if err != nil {
+		t.Errorf("Error of GetVideoInfo: %v", err)
+	}
+}
+
+func testDeleteVideoInfo(t *testing.T) {
+	err := DeleteVideoInfo(tempvid)
+	if err != nil {
+		t.Errorf("Error of DeleteVideoInfo: %v", err)
+	}
+}
+
+func testRegetVideoInfo(t *testing.T) {
+	_, err := GetVideoInfo(tempvid)
+	if err != nil {
+		t.Errorf("Error of RegetVideo: %v", err)
 	}
 }
